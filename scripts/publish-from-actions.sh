84ddc414cca260e8da1142b32a7b7b3a67272693
Nathaniel "Church <church13halo@gmail.com>
2021-08-19 21:38:17 -0400

    Real fix for dumb auth in original config

--- scripts/publish-from-actions.sh
+++ scripts/publish-from-actions.sh
@@ -43,7 +43,7 @@ echo "#############################################"
 echo "######### Setting env vars" 
 echo "#############################################" 
 
-REMOTE_REPO="https://${GITHUB_PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+REMOTE_REPO="https://${GITHUB_ACTOR}:${REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)"
 
 OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)" 
@@ -87,4 +87,4 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 echo `date` >> forcebuild.date
 git add -A 
 git commit -m 'Deploy to GitHub Pages' 
-git push "https://${GITHUB_ACTOR}:${REPO_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" gh-pages
+git push $REMOTE_REPO $REPONAME
